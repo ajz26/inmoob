994da1671eab30d214f9599b6ce06373a56da1d4
Antonio <azambrano@tilomotion.com>
2021-05-24 22:35:29 +0200

    campos nuevos

--- functions/properties_metaboxes.php
+++ functions/properties_metaboxes.php
@@ -24,7 +24,7 @@ new Metabox(
             'icon'  => 'dashicons-images-alt2', 
         ),
         'prices'  => array(
-            'label' => 'Precio',
+            'label' => 'Precios',
             'icon'  => 'dashicons-money-alt',
         ),
         'location' => array(
@@ -33,12 +33,228 @@ new Metabox(
         )
     ),
     'fields' => array(
+
+
+
+        array(
+            'tab'        => 'type',
+            'name'       => 'Tipo de gestión',
+            'id'         => 'gestion_types',
+            'type'       => 'taxonomy',
+            'taxonomy'   => 'gestion_types',
+            'field_type' => 'select',
+            'placeholder' => 'Elige una opción'
+        ),
+
+        array(
+            'tab'        => 'type',
+            'name'       => 'estado de gestión',
+            'id'         => 'gestion_states',
+            'type'       => 'taxonomy',
+            'taxonomy'   => 'gestion_states',
+            'field_type' => 'select',
+            'placeholder' => 'Elige una opción'
+        ),
+
+        array(
+            'tab'       => 'type',
+            'id'        => 'featured',
+            'name'      => 'Destacar propiedad',
+            'type'      => 'switch',                    
+            'style'     => 'rounded',
+            'on_label'  => 'Si',
+            'off_label' => 'No',
+        ),
+
+        // Features
+
+        array(
+            'tab'        => 'features',
+            'name'       => 'Tipo de inmueble',
+            'id'         => 'property_types',
+            'type'       => 'taxonomy',
+            'taxonomy'   => 'property_types',
+            'field_type' => 'select',
+            'placeholder' => 'Elige una opción',
+            'columns'    => 3
+
+        ),
+
+        array(
+            'tab'        => 'features',
+            'name'       => 'Estado de la propiedad',
+            'id'         => 'property_state',
+            'type'       => 'taxonomy',
+            'taxonomy'   => 'property_state',
+            'field_type' => 'select',
+            'placeholder' => 'Elige una opción',
+            'columns'    => 3
+        ),
+
+        array(
+            'tab'        => 'features',
+            'name'       => 'Tamaño del inmueble en m2',
+            'id'         => 'property_size',
+            'type'       => 'text',
+            'placeholder' => 'Indíca el tamaño',
+            'columns'    => 3
+        ),
+        
+        array(
+            'tab'       => 'features',
+            'type'      => 'divider',
+            'columns'   => 12,
+        ),
+        
+
+        array(
+            'tab'        => 'features',
+            'name'       => 'Nº de Habitaciones',
+            'id'         => 'property_rooms',
+            'type'       => 'taxonomy',
+            'taxonomy'   => 'property_rooms',
+            'field_type' => 'select',
+            'placeholder' => 'Elige una opción',
+            'columns'    => 3
+
+        ),
+        array(
+            'tab'        => 'features',
+            'name'       => 'Nº de baños',
+            'id'         => 'property_bathrooms',
+            'type'       => 'taxonomy',
+            'taxonomy'   => 'property_bathrooms',
+            'field_type' => 'select',
+            'placeholder' => 'Elige una opción',
+            'columns'    => 3
+
+        ),
+
+        array(
+            'tab'        => 'features',
+            'name'       => 'Nº de planta',
+            'id'         => 'property_floor',
+            'type'       => 'text',
+            'taxonomy'   => 'property_floor',
+            'placeholder' => 'Elige una opción',
+            'default'       => '1',
+            'columns'    => 3
+        ),
+
+        array(
+            'tab'        => 'features',
+            'type' => 'divider',
+            'columns' => 12,
+        ),
+        
+        array(
+            'tab'       => 'features',
+            'id'        => 'pets',
+            'name'      => 'Permiten mascotas',
+            'type'      => 'switch',                    
+            'style'     => 'rounded',
+            'on_label'  => 'Si',
+            'off_label' => 'No',
+            'columns'    => 2
+        ),
+        array(
+            'tab'       => 'features',
+            'id'        => 'childrens',
+            'name'      => 'Permiten niños',
+            'type'      => 'switch',                    
+            'style'     => 'rounded',
+            'on_label'  => 'Si',
+            'off_label' => 'No',
+            'columns'    => 2
+        ),
+
+        array(
+            'tab'       => 'features',
+            'id'        => 'terrace',
+            'name'      => 'Tiene terraza',
+            'type'      => 'switch',                    
+            'style'     => 'rounded',
+            'on_label'  => 'Si',
+            'off_label' => 'No',
+            'columns'    => 2
+        ),
+
+        array(
+            'tab'       => 'features',
+            'id'        => 'ascensor',
+            'name'      => 'Tiene Ascensor',
+            'type'      => 'switch',                    
+            'style'     => 'rounded',
+            'on_label'  => 'Si',
+            'off_label' => 'No',
+            'columns'    => 2
+        ),
+
+        array(
+            'tab'        => 'features',
+            'type' => 'divider',
+            'columns' => 12,
+        ),
+        
+        
         array(
             'tab'           => 'features',
-            'title'         => 'Tamaño del inmueble en m2',
-            'name'          => 'property_size',
-            'placeholder'   => 'Tamaño en m2',
+            'id'            => 'extras',
+            'name'          => 'Extras',
+            'type'          => 'textarea',
+            'add_button'    => 'Añadir otro',
+            'clone'         => true,
         ),
+        
+        // Prices
+
+      
+        array(
+            'tab'           => 'prices',
+            'name'          => 'Precio',
+            'id'            => 'price',
+            'placeholder'   => 'Precio del inmueble',
+            'std'           => 'a consultar',
+        ),
+        array(
+            'tab'           => 'prices',
+            'name'          => 'Mostrar antes del precio',
+            'id'            => 'price_preffix',
+            'placeholder'   => 'Prefijo',
+
+        ),
+        array(
+            'tab'           => 'prices',
+            'name'          => 'Mostrar despues del precio',
+            'id'            => 'price_suffix',
+            'placeholder'   => 'Sufijo',
+
+        ),
+
+        array(
+            'tab'        => 'location',
+            'name'       => 'Zona',
+            'id'         => 'property_zones',
+            'type'       => 'taxonomy',
+            'taxonomy'   => 'property_zones',
+            'field_type' => 'select_advanced',
+            'placeholder' => 'Elige una opción',
+        ),
+        array(
+            'tab'  => 'location',
+            'name' => 'Dirección',
+            'id'   => 'address',
+            'type' => 'textarea',
+        ),
+        array(
+            'tab'  => 'location',
+            'name' => 'Link de google maps',
+            'id'   => 'gmaps_link',
+            'type' => 'textarea',
+            'rows' => 2,
+            'columns' => 12
+        ),
+
     )
     )
 );
